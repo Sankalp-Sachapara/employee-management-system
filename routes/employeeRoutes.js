32286fbf5e43b07fd1abde8d26c3495bd9ddcd5d
Sankalp Kanubhai Sachapara <101259898+Sankalp-Sachapara@users.noreply.github.com>
2025-04-06 13:48:24 -0400

    Add employeeRoutes.js

---
+++ routes/employeeRoutes.js
@@ -0,0 +1,22 @@
+// routes/employeeRoutes.js - Defines API endpoints
+
+const express = require('express');
+const router = express.Router();
+const employeeController = require('../controllers/employeeController');
+
+// Get all employees
+router.get('/employees', employeeController.getAllEmployees);
+
+// Get employee by ID
+router.get('/employees/:id', employeeController.getEmployeeById);
+
+// Create a new employee
+router.post('/employees', employeeController.createEmployee);
+
+// Update an employee
+router.put('/employees/:id', employeeController.updateEmployee);
+
+// Delete an employee
+router.delete('/employees/:id', employeeController.deleteEmployee);
+
+module.exports = router;
